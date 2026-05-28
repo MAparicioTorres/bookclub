9a42a5c07d7831d7bc721b31fcb478c2feab9ba1
Miguel Aparicio <ma.aparicio.torres@gmail.com>
2026-05-28 17:21:29 +0200

    refactor: move @Setter to class level and adjust precision to 3

--- backend/src/main/java/com/migue/bookclub/model/UserBookRating.java
+++ backend/src/main/java/com/migue/bookclub/model/UserBookRating.java
@@ -1,35 +1,31 @@
 package com.migue.bookclub.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "user_book_rating")
 @Getter
+@Setter
 @NoArgsConstructor
 @ToString
 public class UserBookRating {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private long id;
 
-    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id")
     private Book book;
 
-    @Setter
-    @Column(precision = 4, scale = 2)
+    @Column(precision = 3, scale = 2)
     private BigDecimal rating;
 
     public UserBookRating(User user, Book book, BigDecimal rating) {
