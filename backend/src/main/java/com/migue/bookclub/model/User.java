94a6444ee5579d5475a1c7e6472fb69dc5839152
Miguel Aparicio <ma.aparicio.torres@gmail.com>
2026-05-28 17:21:08 +0200

    refactor: move @Setter to class level

--- backend/src/main/java/com/migue/bookclub/model/User.java
+++ backend/src/main/java/com/migue/bookclub/model/User.java
@@ -13,28 +13,35 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "member") // 'user' conflicts with PostgreSQL as it's a reserved word
 @Getter
+@Setter
 @NoArgsConstructor
 @ToString
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private long id;
 
-    @Setter
+    @Column(nullable = false, unique = true)
     private String username;
 
+    @Setter(AccessLevel.NONE)
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Enumerated(EnumType.STRING)
-    @Setter
+    @Column(nullable = false)
     private Role role = Role.USER; // default new users to USER role
 
     @CreationTimestamp
+    @Setter(AccessLevel.NONE)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
+    @Setter(AccessLevel.NONE)
     private LocalDateTime updatedAt;
 
     public User(String username, String password, String email, Role role) {
