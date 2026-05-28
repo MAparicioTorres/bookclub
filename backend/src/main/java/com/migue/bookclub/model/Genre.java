c52095599c02abe0478564a9a4e9d9da9bfde3a7
Miguel Aparicio <ma.aparicio.torres@gmail.com>
2026-05-28 17:20:20 +0200

    feat: add Genre entity with a @ManyToMany relationship and GenreRepository

--- backend/src/main/java/com/migue/bookclub/model/Genre.java
+++ backend/src/main/java/com/migue/bookclub/model/Genre.java
@@ -1,4 +1,31 @@
 package com.migue.bookclub.model;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "genre")
+@NoArgsConstructor
+@Setter
+@Getter
 public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "genres")
+    private List<Book> books;
+
+    public Genre(String name) {
+        this.name = name;
+    }
 }
--- backend/src/main/java/com/migue/bookclub/repository/GenreRepository.java
+++ backend/src/main/java/com/migue/bookclub/repository/GenreRepository.java
@@ -1,4 +1,9 @@
 package com.migue.bookclub.repository;
 
-public interface Genre {
+import com.migue.bookclub.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
