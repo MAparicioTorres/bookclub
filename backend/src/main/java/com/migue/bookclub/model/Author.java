afc110e621c5a5063d438334f7a0b4f8e113c774
Miguel Aparicio <ma.aparicio.torres@gmail.com>
2026-05-28 17:15:09 +0200

    refactor: move @Setter to class level with AcessLevel.NONE for ID and add constructor

--- backend/src/main/java/com/migue/bookclub/model/Author.java
+++ backend/src/main/java/com/migue/bookclub/model/Author.java
@@ -2,8 +2,10 @@ package com.migue.bookclub.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,14 +13,20 @@ import java.util.List;
 @Entity
 @Table(name = "author")
 @Getter
+@Setter
 @NoArgsConstructor
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private long id;
 
     private String name;
 
     @ManyToMany(mappedBy = "authors")
     private List<Book> books;
+
+    public Author(String name) {
+        this.name = name;
+    }
 }
