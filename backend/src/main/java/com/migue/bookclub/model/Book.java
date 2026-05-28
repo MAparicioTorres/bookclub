d613850f396406e4cd38a288e3b4f52b88ab895f
Miguel Aparicio <ma.aparicio.torres@gmail.com>
2026-05-28 17:22:11 +0200

    refactor: move @Setter to class level, remove redundant parenthesis and update constructor

--- backend/src/main/java/com/migue/bookclub/model/Book.java
+++ backend/src/main/java/com/migue/bookclub/model/Book.java
@@ -2,29 +2,37 @@ package com.migue.bookclub.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
 @Table(name = "book")
 @Getter
+@Setter
 @NoArgsConstructor
 @ToString
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private long id;
 
-    @Setter
+    @Column(nullable = false)
     private String title;
 
-    @ManyToMany()
+    private LocalDate publishedDate;
+
+    private LocalDate startDate;
+
+    private LocalDate finishDate;
+
+    private String coverUrl;
+
+    @ManyToMany
     @JoinTable(
             name = "book_author",
             joinColumns = @JoinColumn(name = "book_id"),
@@ -32,11 +40,22 @@ public class Book {
     )
     private List<Author> authors;
 
+    @ManyToMany
+    @JoinTable(
+            name = "book_genre",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id")
+    )
+    private List<Genre> genres;
+
     @Transient
-    @Setter
     private BigDecimal avgRating;
 
-    public Book(String title) {
+    public Book(String title, LocalDate publishedDate, LocalDate startDate, LocalDate finishDate, String coverUrl) {
         this.title = title;
+        this.publishedDate = publishedDate;
+        this.startDate = startDate;
+        this.finishDate = finishDate;
+        this.coverUrl = coverUrl;
     }
 }
