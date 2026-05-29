c014ac3c57caa36918afe24dac7756b97b88a8af
Miguel Aparicio <ma.aparicio.torres@gmail.com>
2026-05-29 16:36:34 +0200

    feat: add /login endpoint

--- backend/src/main/java/com/migue/bookclub/controller/AuthController.java
+++ backend/src/main/java/com/migue/bookclub/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.migue.bookclub.controller;
 
+import com.migue.bookclub.dto.LoginRequest;
 import com.migue.bookclub.dto.LoginResponse;
 import com.migue.bookclub.dto.RegisterRequest;
+import com.migue.bookclub.model.User;
+import com.migue.bookclub.security.JwtService;
 import com.migue.bookclub.service.AuthService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,19 +15,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
-
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
 
     @PostMapping("/register")
     public ResponseEntity<LoginResponse> register(@Valid @RequestBody RegisterRequest registerRequest){
         LoginResponse loginResponse = authService.register(registerRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(loginResponse);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        LoginResponse loginResponse = authService.login(loginRequest);
+        return ResponseEntity.ok(loginResponse);
     }
 }
