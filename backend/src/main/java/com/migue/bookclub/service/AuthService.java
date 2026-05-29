1080e846c16528b9b53f8649c83ee2b2d793a51d
Miguel Aparicio <ma.aparicio.torres@gmail.com>
2026-05-29 16:38:33 +0200

    feat: add 'login' behavior and implement @RequiredArgsConstructor to reduce boilerplate code

--- backend/src/main/java/com/migue/bookclub/service/AuthService.java
+++ backend/src/main/java/com/migue/bookclub/service/AuthService.java
@@ -1,23 +1,26 @@
 package com.migue.bookclub.service;
 
+import com.migue.bookclub.dto.LoginRequest;
 import com.migue.bookclub.dto.LoginResponse;
 import com.migue.bookclub.dto.RegisterRequest;
 import com.migue.bookclub.enums.Role;
+import com.migue.bookclub.exception.InvalidCredentialsException;
 import com.migue.bookclub.model.User;
 import com.migue.bookclub.repository.UserRepository;
+import com.migue.bookclub.security.JwtService;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    private final JwtService jwtService;
 
     @Transactional
     public LoginResponse register(RegisterRequest request) {
@@ -38,10 +41,32 @@ public class AuthService {
 
         User savedUser = userRepository.save(user);
 
+        String token = jwtService.generateToken(user.getUsername());
+
         return new LoginResponse(
                 savedUser.getUsername(),
                 savedUser.getRole().toString(),
-                "TOKEN_TEST"
+                token
+        );
+    }
+
+    public LoginResponse login(LoginRequest loginRequest) {
+        Optional<User> userOpt = userRepository.findByUsername(loginRequest.getUsername());
+        if (userOpt.isEmpty()) {
+            throw new InvalidCredentialsException("Invalid username or password");
+        }
+
+        User user = userOpt.get();
+        String username = user.getUsername();
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new InvalidCredentialsException("Invalid username or password");
+        }
+
+        return new LoginResponse(
+                username,
+                user.getRole().name(),
+                jwtService.generateToken(username)
         );
     }
 }
