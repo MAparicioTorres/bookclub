0d006066dcfcfb2709869d15d9af60400db188e6
Miguel Aparicio <ma.aparicio.torres@gmail.com>
2026-06-01 21:08:04 +0200

    feat: implement custom query for checking if a certain book exists based on its title ignoring cases and spaces

--- backend/src/main/java/com/migue/bookclub/repository/BookRepository.java
+++ backend/src/main/java/com/migue/bookclub/repository/BookRepository.java
@@ -1,9 +1,19 @@
 package com.migue.bookclub.repository;
 
 import com.migue.bookclub.model.Book;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query("""
+            SELECT COUNT(b) > 0 FROM Book b
+            WHERE REPLACE(LOWER(b.title), ' ', '') = REPLACE(LOWER(:title), ' ', '')
+            """)
+    boolean existsByTitleIgnoringSpaces(@Param("title") String title);
 }
