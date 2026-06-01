726329b8a239d51b04361195ea6a969475a04c73
Miguel Aparicio <ma.aparicio.torres@gmail.com>
2026-06-01 14:20:35 +0200

    feat: add a new findByBookId method with @EntityGraph to retrieve the rating along with the user

--- backend/src/main/java/com/migue/bookclub/repository/RatingRepository.java
+++ backend/src/main/java/com/migue/bookclub/repository/RatingRepository.java
@@ -1,10 +1,18 @@
 package com.migue.bookclub.repository;
 
 import com.migue.bookclub.model.UserBookRating;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RatingRepository extends JpaRepository<UserBookRating, Long> {
+
+    @EntityGraph(attributePaths = "user")
+    List<UserBookRating> findByBookId(@Param("bookId") Long bookId);
 
 }
