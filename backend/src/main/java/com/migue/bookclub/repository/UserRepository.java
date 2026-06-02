a82555e000a89a2fd622e66b2d94dc739f78ebc5
Miguel Aparicio <ma.aparicio.torres@gmail.com>
2026-06-02 18:17:43 +0200

    feat: add new derived query for checking if an email already exists

--- backend/src/main/java/com/migue/bookclub/repository/UserRepository.java
+++ backend/src/main/java/com/migue/bookclub/repository/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     boolean existsByUsernameIgnoreCase(String username);
+
+    boolean existsByEmail(String email);
 }
