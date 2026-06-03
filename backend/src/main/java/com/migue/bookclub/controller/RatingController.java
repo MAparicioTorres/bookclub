00c321bd7e6ae90e055d5691b6c4ad9eb922c24b
Miguel Aparicio <ma.aparicio.torres@gmail.com>
2026-06-03 11:52:42 +0200

    feat: add new endpoint for updating a rating through the book ID

--- backend/src/main/java/com/migue/bookclub/controller/RatingController.java
+++ backend/src/main/java/com/migue/bookclub/controller/RatingController.java
@@ -1,9 +1,9 @@
 package com.migue.bookclub.controller;
 
-import com.migue.bookclub.dto.RatingRequest;
+import com.migue.bookclub.dto.CreateRatingRequest;
 import com.migue.bookclub.dto.RatingResponse;
+import com.migue.bookclub.dto.UpdateRatingRequest;
 import com.migue.bookclub.model.User;
-import com.migue.bookclub.repository.RatingRepository;
 import com.migue.bookclub.service.RatingService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,8 +22,18 @@ public class RatingController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public RatingResponse rateBook(
             @AuthenticationPrincipal User user,
-            @Valid @RequestBody RatingRequest request
+            @Valid @RequestBody CreateRatingRequest request
     ) {
         return ratingService.rateBook(user, request);
     }
+
+    @PutMapping("/{bookId}")
+    public RatingResponse updateRating(
+            @AuthenticationPrincipal User user,
+            @PathVariable long bookId,
+            @RequestBody @Valid UpdateRatingRequest request
+    ) {
+        return ratingService.updateRating(user, bookId, request);
+    }
+
 }
