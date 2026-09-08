aa9a4acdd00753c1be418c708f469c4d7adcfd64
Miguel Ángel <ma.aparicio.torres@gmail.com>
2026-09-08 18:07:42 +0200

    feat: add ProfileResponse for retrieving the user information

--- frontend/src/types/auth.ts
+++ frontend/src/types/auth.ts
@@ -1,16 +1,23 @@
 export interface LoginRequest {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 export interface LoginResponse {
-    username: string;
-    role: string;
-    token: string;
+  username: string;
+  role: string;
+  token: string;
 }
 
-export interface RegisterRequest{
-    username: string;
-    email: string;
-    password: string;
+export interface RegisterRequest {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface ProfileResponse {
+  username: string;
+  email: string;
+  role: string;
+  createdAt: string;
 }
