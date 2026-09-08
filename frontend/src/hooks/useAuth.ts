dcfbaf5bdd789166170013de9f53dd6cd63b8b57
Miguel Ángel <ma.aparicio.torres@gmail.com>
2026-09-08 18:36:06 +0200

    feat(frontend): add useLogout hook for removing the token and redirecting the user to /login

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,21 +1,44 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login, register } from "../api/auth";
-import type { LoginRequest, RegisterRequest } from "../types/auth";
+import type {
+  LoginRequest,
+  LoginResponse,
+  RegisterRequest,
+} from "../types/auth";
+import type { AxiosError } from "axios";
 
-export function useLogin(){
-    return useMutation({
-        mutationFn: (data: LoginRequest) => login(data),
-        onSuccess: (res) => {
-            localStorage.setItem('jwt', res.token);
-        }
-    });
+export function useLogin() {
+  return useMutation<
+    LoginResponse,
+    AxiosError<{ message: string }>,
+    LoginRequest
+  >({
+    mutationFn: (data) => login(data),
+    onSuccess: (res) => {
+      localStorage.setItem("jwt", res.token);
+    },
+  });
 }
 
-export function useRegister(){
-    return useMutation({
-        mutationFn: (data: RegisterRequest) => register(data),
-        onSuccess: (res) => {
-            localStorage.setItem('jwt', res.token);
-        }
-    })
+export function useRegister() {
+  return useMutation<
+    LoginResponse,
+    AxiosError<{ message: string }>,
+    RegisterRequest
+  >({
+    mutationFn: (data) => register(data),
+    onSuccess: (res) => {
+      localStorage.setItem("jwt", res.token);
+    },
+  });
+}
+
+export function useLogout() {
+  const queryClient = useQueryClient();
+
+  return () => {
+    localStorage.removeItem("jwt");
+    queryClient.clear();
+    window.location.href = "/login";
+  };
 }
