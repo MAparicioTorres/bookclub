d8e466497b2360aef33943a6b6c4755ac651009c
Miguel Ángel <ma.aparicio.torres@gmail.com>
2026-09-08 18:36:44 +0200

    feat(frontend): add getBooks endpoint

--- frontend/src/api/books.ts
+++ frontend/src/api/books.ts
@@ -1,12 +1,23 @@
 import apiClient from "./client";
-import type { BookResponse, BookDetailResponse, CreateBookRequest } from "../types/book";
+import type {
+  BookResponse,
+  BookDetailResponse,
+  CreateBookRequest,
+} from "../types/book";
+
+export const getBooks = async (): Promise<BookResponse[]> => {
+  const response = await apiClient.get(`/books`);
+  return response.data;
+};
 
 export const getBook = async (id: number): Promise<BookDetailResponse> => {
-    const response = await apiClient.get(`/books/${id}`);
-    return response.data;
-}
+  const response = await apiClient.get(`/books/${id}`);
+  return response.data;
+};
 
-export const createBook = async (data: CreateBookRequest): Promise<BookResponse> => {
-    const response = await apiClient.post('/books', data);
-    return response.data; 
-} 
+export const createBook = async (
+  data: CreateBookRequest,
+): Promise<BookResponse> => {
+  const response = await apiClient.post("/books", data);
+  return response.data;
+};
