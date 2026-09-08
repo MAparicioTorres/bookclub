b34325c57239b033291b450b6f82ba18fd79f834
Miguel Ángel <ma.aparicio.torres@gmail.com>
2026-09-08 18:21:22 +0200

    feat(frontend): create input and button components for the form

--- frontend/src/components/FormButton.tsx
+++ frontend/src/components/FormButton.tsx
@@ -5,15 +5,22 @@ interface FormButtonProps {
   onClick?: () => void;
   type?: "submit" | "button";
   attributes?: string;
+  disabled?: boolean;
 }
 
-function FormButton({ children, onClick, type, attributes }: FormButtonProps) {
+function FormButton({
+  children,
+  onClick,
+  type,
+  attributes,
+  disabled,
+}: FormButtonProps) {
   return (
-   
     <button
       type={type}
       onClick={onClick}
-      className={`relative px-4 py-5 rounded-xl bg-primary text-white text-xl hover:opacity-90 cursor-pointer ${attributes}`}
+      className={`relative px-4 py-5 rounded-xl bg-primary text-white text-xl cursor-pointer hover:opacity-90 disabled:cursor-not-allowed ${attributes}`}
+      disabled={disabled}
     >
       <span className="absolute m-px inset-0 rounded-xl border-t border-t-white/40 border-b-6 border-b-black/30" />
       {children}
--- frontend/src/components/FormInput.tsx
+++ frontend/src/components/FormInput.tsx
@@ -3,9 +3,20 @@ interface FormInputProps {
   name: string;
   id: string;
   type: string;
+  isRequired?: boolean;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-function FormInput({ label, name, id, type }: FormInputProps) {
+function FormInput({
+  label,
+  name,
+  id,
+  type,
+  isRequired,
+  value,
+  onChange,
+}: FormInputProps) {
   return (
     <div>
       <label htmlFor={id} className="text-white">
@@ -15,16 +26,19 @@ function FormInput({ label, name, id, type }: FormInputProps) {
         name={name}
         id={id}
         type={type}
-        className="
-         w-full
-          border-2 border-white/40 rounded
-          py-2 px-3
-          bg-transparent text-white
-          transition-colors duration-200
-          outline-2
-          outline-white/0
-          input
-"
+        required={isRequired}
+        value={value}
+        onChange={onChange}
+        className={`
+           w-full
+            border-2 border-gray-500 rounded
+            py-2 px-3
+            bg-transparent text-white
+            transition-colors duration-200
+            outline-2
+            outline-white/0
+            input
+        `}
       />
     </div>
   );
